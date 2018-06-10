cf303aecf7e397f822c0267c698b350fe3ff600d
Ling Jiang <kyooryoo@hotmail.com>
2018-06-10 14:18:27 +0800

    fixed db issues in user model

--- models/user.py
+++ models/user.py
@@ -13,6 +13,10 @@ class UserModel(db.Model):
         self.username = username
         self.password = password
 
+    def save_to_db(self):
+        db.session.add(self)
+        db.session.commit()
+
     @classmethod
     def find_by_username(cls, name):
         return cls.query.filter_by(username=name).first()
